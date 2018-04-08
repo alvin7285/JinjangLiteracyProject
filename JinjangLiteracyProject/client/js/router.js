30a827881c4df0e627d633507bb3299e74908fda
alvin7285 <31263725+alvin7285@users.noreply.github.com>
2018-04-09 01:28:46 +0800

    done with iron routing

--- JinjangLiteracyProject/client/js/router.js
+++ JinjangLiteracyProject/client/js/router.js
@@ -8,6 +8,31 @@ Router.map(function(){
     template:'home'
   });
 
+  this.route('aboutUs', {
+    path: '/aboutUs',
+    template:'aboutUs'
+  });
+
+  this.route('events', {
+    path: '/events',
+    template:'events'
+  });
+
+  this.route('donate', {
+    path: '/donate',
+    template:'donate'
+  });
+
+  this.route('volunteer', {
+    path: '/volunteer',
+    template:'volunteer'
+  });
+
+  this.route('jinjangStory', {
+    path: '/jinjangStory',
+    template:'jinjangStory'
+  });
+
   this.route('login', {
     path: '/login',
     template:'login'
